ad87a746b16a21e3ada33d52d6e7d96358e99452
Dimas-Saputra-ME <saputra.dinas@yahoo.co.id>
2023-05-20 20:36:35 +0700

    [FIX] fixing date database

--- app/src/main/java/com/example/asah/Eat.kt
+++ app/src/main/java/com/example/asah/Eat.kt
@@ -50,11 +50,12 @@ class Eat : AppCompatActivity() {
 
         val year = c.get(Calendar.YEAR).toString()
         val month = c.get(Calendar.MONTH).toString()
-        val day = c.get(Calendar.DAY_OF_WEEK)
+        val day_week = c.get(Calendar.DAY_OF_WEEK)
+        val day = c.get(Calendar.DAY_OF_MONTH)
 
         val hari = arrayOf("Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu")
 
-        val date = "${hari[day-1]} - $month - $year"
+        val date = "${hari[day_week-1]} - $day/${month.toInt() + 1}/$year"
 
         return date
     }
